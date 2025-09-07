f9c08e260ee6ea6d6ea82ff47535cf0e96785a4a
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-07 23:09:16 +0700

    change input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ function Input({
   return (
     <input
       type={type}
-      value={value || ''}
+      value={value ?? ''}
       data-slot='input'
       className={cn(
         'md:text-md-regular text-sm-regular bg-neutral-25 focus-visible:ring-primary-200 h-12 w-full rounded-xl border-1 border-neutral-200 px-3 py-2 leading-5 text-neutral-700 transition-all outline-none focus-visible:ring-1 md:h-14 md:px-4 md:py-2.25 md:leading-7.5',
